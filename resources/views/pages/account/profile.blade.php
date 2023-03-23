c1a9f9f565ba2e0af453b4ff2a3d6876e03b2189
lekhuong1906 <lekhuong190602@gmail.com>
2023-03-23 08:17:35 +0700

    second commit

--- resources/views/pages/account/profile.blade.php
+++ resources/views/pages/account/profile.blade.php
@@ -1,15 +1,4 @@
 @extends('pages.layout')
 @section('content')
-    <div class="row">
-        <div class="content">
-            <div class="card mb-3">
-                <img src="" class="card-img-top avatar" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Customer</h5>
-                    <p class="card-text"></p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    </div>
+
 @endsection
